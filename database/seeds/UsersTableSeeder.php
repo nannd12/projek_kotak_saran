66c591cbbd941430d0ab769efbe9a61777144eac
Kevin Hendrawan Hartono <amedia92@gmail.com>
2017-11-28 17:07:13 +0700

    perubahan seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,11 +12,20 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         //menjalankan seeder
-        DB::table('users')->insert([
+        DB::table('users')->insert(array(
+            [
         	'name' => 'Kevin Hendrawan Hartono',
         	'nim' => '150602031',
         	'email' => 'amedia92@gmail.com',
         	'password' => bcrypt('12345678'),
-        	]);
+        	],
+            [
+            'name' => 'Ananda Dwi Prasetya',
+            'nim' => '150602025',
+            'email' => 'nandadp@gmail.com',
+            'password' => bcrypt('12345678'),
+            ],
+            )
+        );
     }
 }
